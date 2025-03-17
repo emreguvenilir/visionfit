9f7c6041cec0bce9f1d3caf97e82f9c4d485d4d0
emreguvenilir <50936821+emreguvenilir@users.noreply.github.com>
2025-03-16 22:06:13 -0400

    Working backend

---
+++ backend/src/main/java/com/visionfit/VisionFitBackendApplication.java
@@ -0,0 +1,11 @@
+package com.visionfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionFitBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VisionFitBackendApplication.class, args);
+    }
+}
